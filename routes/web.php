2a47d845674f9ef8bf2526838dcf37cdc0d62dbc
Bruno Henrique <brunohenrique242@gmail.com>
2023-08-16 13:05:43 -0300

    chore: show method created

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
 Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
 Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 
 Route::get('/contato', function () {
