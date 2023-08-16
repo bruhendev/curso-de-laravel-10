79fdea61946f654c3b752e7b02ed260128cbfa03
Bruno Henrique <brunohenrique242@gmail.com>
2023-08-16 15:03:39 -0300

    chore: create validations

--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,8 +1,14 @@
 <h1>Nova Dúvida</h1>
 
+@if ($errors->any())
+    @foreach ($errors->all() as $error)
+        {{ $error }}
+    @endforeach
+@endif
+
 <form action="{{ route('supports.store') }}" method="POST">
     @csrf
-    <input type="text" placeholder="Assunto" name="subject"><br><br>
-    <textarea name="body"  cols="30" rows="5" placeholder="Descrição"></textarea><br><br>
+    <input type="text" placeholder="Assunto" name="subject" value="{{ old('subject') }}"><br><br>
+    <textarea name="body"  cols="30" rows="5" placeholder="Descrição">{{ old('body') }}</textarea><br><br>
     <button type="submit">Enviar</button>
 </form>
