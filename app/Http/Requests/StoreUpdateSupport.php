9e1cadeb19cf7d95dbd130bc8ce1573e8df91117
Bruno Henrique <brunohenrique242@gmail.com>
2023-08-17 11:31:31 -0300

    chore: update StoreUpdateSupport

--- app/Http/Requests/StoreUpdateSupport.php
+++ app/Http/Requests/StoreUpdateSupport.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreUpdateSupport extends FormRequest
 {
@@ -27,7 +28,13 @@ class StoreUpdateSupport extends FormRequest
         ];
 
         if ($this->method() === 'PUT') {
-            $rules['subject'] = ['required', 'min:3', 'max:255', "unique:supports,subject,{$this->id},id"];
+            $rules['subject'] = [
+                'required',
+                'min:3',
+                'max:255',
+                // "unique:supports,subject,{$this->id},id"
+                Rule::unique('supports')->ignore($this->id)
+            ];
         }
 
         return $rules;
